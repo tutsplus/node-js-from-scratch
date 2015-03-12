9a47d954f9af88e8fc9210875b9f4589b48e3115
Adam Brown <adamarthurryan@gmail.com>
2015-03-12 14:06:47 -0400

    update server.js for better commenting

--- 3-7-web-server/server.js
+++ 3-7-web-server/server.js
@@ -6,13 +6,16 @@ var server = http.createServer(function (request, response) {
   response.write('<p>URL: ' + request.url + '<p>');
   response.end();
 });
-server.listen(3000);
 
-// Uncomment this code to pipe a file stream to the response.
+// Uncomment this line to echo some information about the request
+//server.listen(3000);
 
-// var fs = require('fs');
-// var server2 = http.createServer();
-// server2.on('request', function (req, res) {
-//   fs.createReadStream('server.js').pipe(res);
-// });
-// server2.listen(3000);
+
+var fs = require('fs');
+  var server2 = http.createServer();
+  server2.on('request', function (req, res) {
+  fs.createReadStream('server.js').pipe(res);
+});
+
+// Uncomment this line to pipe a file stream to the response.
+server2.listen(3000);
